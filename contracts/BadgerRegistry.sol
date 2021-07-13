f5c2eed1d5997d90b1a747a492ca80c75b5cb840
Entreprenerd <valeranijr@gmail.com>
2021-07-13 18:19:17 +0200

    feat: registry uses EnumerableSet + sends back more data

--- contracts/BadgerRegistry.sol
+++ contracts/BadgerRegistry.sol
@@ -2,11 +2,33 @@
 pragma solidity >=0.6.0 <0.7.0;
 pragma experimental ABIEncoderV2;
 
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+
+// Data from Vault
+struct StrategyParams {
+  uint256 performanceFee;
+  uint256 activation;
+  uint256 debtRatio;
+  uint256 minDebtPerHarvest;
+  uint256 maxDebtPerHarvest;
+  uint256 lastReport;
+  uint256 totalDebt;
+  uint256 totalGain;
+  uint256 totalLoss;
+  bool enforceChangeLimit;
+  uint256 profitLimitRatio;
+  uint256 lossLimitRatio;
+  address customCheck;
+}
+
 interface VaultView {
   function name() external view returns (string memory);
   function symbol() external view returns (string memory);
 
   function token() external view returns (address);
+
+  function strategies(address _strategy) external view returns (StrategyParams memory);
+
 
   function pendingGovernance() external view returns (address);
   function governance() external view returns (address);
@@ -29,18 +51,29 @@ interface StratView {
 
 
 contract BadgerRegistry {
+  using EnumerableSet for EnumerableSet.AddressSet;
+
+
   // Multisig. Vaults from here are considered Production ready
   address GOVERNANCE = 0xB65cef03b9B89f99517643226d76e286ee999e77;
 
   // Given an Author Address, and Token, Return the Vault
-  mapping(address => address[]) public vaults;
+  mapping(address => EnumerableSet.AddressSet) private vaults;
 
   event NewVault(address author, address vault);
   event RemoveVault(address author, address vault);
   event PromoteVault(address author, address vault);
 
-  // Data from Vault
-  struct StrategyParams {
+
+
+  // View Data for each strat we will return
+  struct StratInfo {
+    string name;
+
+    address strategist;
+    address rewards;
+    address keeper;
+
     uint256 performanceFee;
     uint256 activation;
     uint256 debtRatio;
@@ -54,29 +87,6 @@ contract BadgerRegistry {
     uint256 profitLimitRatio;
     uint256 lossLimitRatio;
     address customCheck;
-  }
-
-  // View Data for each strat we will return
-  struct StratInfo {
-    string name;
-
-    address strategist;
-    address rewards;
-    address keeper;
-
-    // uint256 performanceFee;
-    // uint256 activation;
-    // uint256 debtRatio;
-    // uint256 minDebtPerHarvest;
-    // uint256 maxDebtPerHarvest;
-    // uint256 lastReport;
-    // uint256 totalDebt;
-    // uint256 totalGain;
-    // uint256 totalLoss;
-    // bool enforceChangeLimit;
-    // uint256 profitLimitRatio;
-    // uint256 lossLimitRatio;
-    // address customCheck;
   }
 
   // Vault data we will return for each Vault
@@ -99,40 +109,32 @@ contract BadgerRegistry {
 
   // Anyone can add a vault to here
   function add(address vault) public {
-    vaults[msg.sender].push(vault);
+    vaults[msg.sender].add(vault);
     
     emit NewVault(msg.sender, vault);
   }
 
   function remove(address vault) public {
-    address[] storage list = vaults[msg.sender];
-    uint256 length = list.length;
-    uint256 index = length; // Index can only be up to length - 1. If index if lenght, we didn't find it
-    
-    for(uint256 x; x < length; x++){
-      if(list[x] == vault){
-        index = x;
-      }
-    }
-
-    if(index != length) {
-      list[index] = list[list.length - 1];
-      list.pop();
-    }
+    vaults[msg.sender].remove(vault);
 
     emit RemoveVault(msg.sender, vault);
   }
 
   function fromAuthor(address author) public view returns (address[] memory) {
-    address[] memory list = vaults[msg.sender];
+    uint256 length = vaults[author].length();
+    address[] memory list = new address[](length);
+    for (uint256 i = 0; i < length; i++) {
+      list[i] = vaults[author].at(i);
+    }
     return list;
   }
 
   function fromAuthorVaults(address author) public view returns (VaultInfo[] memory) {
-    address[] memory list = vaults[msg.sender];
-    VaultInfo[] memory vaultData = new VaultInfo[](list.length);
-    for(uint x = 0; x < list.length; x++){
-      VaultView vault = VaultView(list[x]);
+    uint256 length = vaults[author].length();
+
+    VaultInfo[] memory vaultData = new VaultInfo[](length);
+    for(uint x = 0; x < length; x++){
+      VaultView vault = VaultView(vaults[author].at(x));
       StratInfo[] memory allStrats = new StratInfo[](0);
 
       VaultInfo memory data = VaultInfo({
@@ -149,16 +151,17 @@ contract BadgerRegistry {
 
       vaultData[x] = data;
     }
+    return vaultData;
   }
 
-  
+
   // Given one Author, retrieve all their vaults and the strats associated with them
   function fromAuthorWithDetails(address author) public view returns (VaultInfo[] memory) {
-    address[] memory list = vaults[msg.sender];
-    VaultInfo[] memory vaultData = new VaultInfo[](list.length);
+    uint256 length = vaults[author].length();
+    VaultInfo[] memory vaultData = new VaultInfo[](length);
     
-    for(uint x = 0; x < list.length; x++){
-      VaultView vault = VaultView(list[x]);
+    for(uint x = 0; x < length; x++){
+      VaultView vault = VaultView(vaults[author].at(x));
 
       // TODO: Strat Info with real data
       uint stratCount = 0;
@@ -171,11 +174,26 @@ contract BadgerRegistry {
 
       for(uint z = 0; z < stratCount; z++){
         StratView strat = StratView(vault.withdrawalQueue(z));
+        StrategyParams memory params = vault.strategies(vault.withdrawalQueue(z));
         StratInfo memory stratData = StratInfo({
           name: strat.name(),
           strategist: strat.strategist(),
           rewards: strat.rewards(),
-          keeper: strat.keeper()
+          keeper: strat.keeper(),
+
+          performanceFee: params.performanceFee,
+          activation: params.activation,
+          debtRatio: params.debtRatio,
+          minDebtPerHarvest: params.minDebtPerHarvest,
+          maxDebtPerHarvest: params.maxDebtPerHarvest,
+          lastReport: params.lastReport,
+          totalDebt: params.totalDebt,
+          totalGain: params.totalGain,
+          totalLoss: params.totalLoss,
+          enforceChangeLimit: params.enforceChangeLimit,
+          profitLimitRatio: params.profitLimitRatio,
+          lossLimitRatio: params.lossLimitRatio,
+          customCheck: params.customCheck
         });
         allStrats[z] = stratData;
       }
@@ -202,7 +220,7 @@ contract BadgerRegistry {
   function promote(address vault) public {
     // TODO: Add security checks
     require(msg.sender == GOVERNANCE, "!gov");
-    vaults[msg.sender].push(vault);
+    vaults[msg.sender].add(vault);
 
     emit PromoteVault(msg.sender, vault);
   }
